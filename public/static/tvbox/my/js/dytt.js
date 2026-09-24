aa0bcb6e57d2cfbbf63b924733536616fae22048
wangbin <wangbin@catsti.com>
2026-09-24 17:10:45 +0800

    1）TVBox 配置新增电影天堂采集源。

---
+++ public/static/tvbox/my/js/dytt.js
@@ -0,0 +1,11 @@
+// http://caiji.dyttzyapi.com/api.php/provide/vod/?ac=list
+
+var rule = {
+    模板: '采集1',
+    title: '电影天堂[JS]',
+    host: 'http://caiji.dyttzyapi.com',
+    // homeTid: '13',
+    homeTid: '',
+    cate_exclude: '电影片|连续剧|综艺片|动漫片|电影解说|体育|新闻资讯',
+    parse_url: '',
+}
